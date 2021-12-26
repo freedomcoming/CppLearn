1549881f958312e14098dfed15c1093ddbb1301c
karl <test@test.com>
2021-12-26 22:23:03 +0800

    type: FIX

--- ForUse.cpp
+++ ForUse.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(void)
+int
+main (void)
 {
-    // 初始值 执行测试 执行循环操作 更新用于测试的值      
-    for(int i = 0;i<10;i++){
-        cout << i << endl;
+  // 初始值 执行测试 执行循环操作 更新用于测试的值
+  for (int i = 10; i; i = i - 2)
+    {
+      cout << i << endl;
     }
-    return 0;
+  int x;
+  cout << (x = 100) << endl;
+  cout << (x < 1) << endl;
+  cout << (x > 1) << endl;
+
+  // 倒叙输出字符串
+  string s = "sssb";
+  for (int i = s.size () - 1; i >= 0; i--)
+    {
+      cout << s[i] << endl;
+    }
+
+    // a++ ++a  前置函数销量更高
+  return 0;
 }
